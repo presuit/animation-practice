01b73990b62eb5d31fa7853f112c9f337f3925a7
Yeongho Choi <chldudgh8256@naver.com>
2022-03-30 21:01:07 +0900

    card-scatter card design modify

--- src/routes/CardScatter.tsx
+++ src/routes/CardScatter.tsx
@@ -8,9 +8,7 @@ const INTERVAL_TIME = 250;
 
 export default function CardScatter() {
   const [lid, setLid] = useState(0);
-  const [intervalTime, setIntervalTime] = useState<number | null>(
-    INTERVAL_TIME
-  );
+  const [intervalTime, setIntervalTime] = useState<number | null>(null);
   useInterval(() => {
     setLid((prev) => prev + 1);
   }, intervalTime);
@@ -26,19 +24,19 @@ export default function CardScatter() {
       <aside className="absolute top-0 left-0">
         <span className="text-2xl text-white">{lid}</span>
       </aside>
-      <main className={`flex flex-wrap py-5 justify-center items-start`}>
+      <main className={`flex flex-wrap p-5 justify-center items-start mx-auto`}>
         {data.map((i, _) =>
           i + 1 <= lid ? (
             <motion.div
               layoutId={i + 1 + ""}
-              className="w-80 h-80 p-5"
+              className="w-60 aspect-square p-5"
               key={i + 1}
             >
               <div
                 onClick={() => {
                   alert(i + 1);
                 }}
-                className="w-full h-full bg-slate-800 rounded-2xl flex justify-center items-center text-2xl text-white font-semibold cursor-pointer"
+                className="w-full h-full bg-slate-800 rounded-2xl flex justify-center items-center text-2xl text-white font-semibold cursor-pointer hover:bg-slate-900 transition-colors ring-2 ring-slate-700 ring-offset-4 ring-offset-slate-900"
               >
                 {i + 1}
               </div>
@@ -47,18 +45,36 @@ export default function CardScatter() {
         )}
       </main>
       {lid === data.length ? null : (
-        <aside className="w-full h-screen fixed top-0 left-0">
-          {reversedData.map((i, _) => (
-            <motion.div
-              layoutId={i + 1 + ""}
-              className="w-80 h-80 p-5 absolute top-0 left-0 right-0 bottom-0 m-auto"
-              key={i + 1}
-            >
-              <div className="w-full h-full bg-slate-700 rounded-2xl flex justify-center items-center text-2xl font-semibold text-white">
-                {i + 1}
-              </div>
-            </motion.div>
-          ))}
+        <aside className="w-full h-screen absolute top-0 left-0">
+          {reversedData.map((i, _) => {
+            if (i + 1 === 1) {
+              return (
+                <motion.div
+                  layoutId={i + 1 + ""}
+                  className="w-80 h-80 p-5 absolute top-0 left-0 right-0 bottom-0 m-auto"
+                  key={i + 1}
+                >
+                  <div
+                    onClick={() => setIntervalTime(INTERVAL_TIME)}
+                    className="w-full h-full bg-slate-800 rounded-2xl flex justify-center items-center text-2xl font-semibold  cursor-pointer text-white ring-4 ring-offset-4 ring-offset-slate-900 ring-slate-700 transition-all hover:bg-slate-900"
+                  >
+                    {i + 1}
+                  </div>
+                </motion.div>
+              );
+            }
+            return (
+              <motion.div
+                layoutId={i + 1 + ""}
+                className="w-80 aspect-square p-5 absolute top-0 left-0 right-0 bottom-0 m-auto"
+                key={i + 1}
+              >
+                <div className="w-full h-full bg-slate-800 rounded-2xl flex justify-center items-center text-2xl font-semibold text-white ring-4 ring-offset-4 ring-offset-slate-900 ring-slate-700">
+                  {i + 1}
+                </div>
+              </motion.div>
+            );
+          })}
         </aside>
       )}
     </div>
